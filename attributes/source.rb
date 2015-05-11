139f6a150b736018aee520511595581b2361d3bc
alex <uohzxela@gmail.com>
2015-05-11 18:03:29 +0800

    update source.rb to install kafka plugin and library

--- attributes/source.rb
+++ attributes/source.rb
@@ -7,7 +7,7 @@ default["heka"]["source"] = {
   "packages" => %w{ cmake mercurial git-core },
   "plugins" => [
     # Add items here like the following that's required for integration with kafka
-    # "add_external_plugin(git https://github.com/innerr/heka-kafka 4cd1287b3ea19d0a04c807e9bed5a0cfb92cc805)"
+    "add_external_plugin(git https://github.com/genx7up/heka-kafka acf3ac7a3d6d6dab313510f81828fca2f9375229)"
   ],
   "git" => {
     "source" => "https://github.com/mozilla-services/heka.git",
@@ -20,7 +20,7 @@ default["heka"]["source"] = {
     "packages" => [
       ## Add libraries required for plugins such as the the sarama library
       ## for communication with kafka
-      # "github.com/Shopify/sarama"
+      "github.com/Shopify/sarama"
     ]
   },
   "dir" => {
