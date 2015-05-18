7a4f8f3c0528607b6e04259f4bafb451c8123188
alex <uohzxela@gmail.com>
2015-05-18 16:10:10 +0800

    remove stray comma

--- resources/from_kafka_to_s3.rb
+++ resources/from_kafka_to_s3.rb
@@ -6,6 +6,6 @@ attribute :secret_key, :kind_of => String
 attribute :bucket, :kind_of => String
 attribute :prefix, :kind_of => String
 attribute :region, :kind_of => String
-attribute :topic, :kind_of => String, , :name_attribute => true
+attribute :topic, :kind_of => String, :name_attribute => true
 attribute :ticker_interval, :kind_of => Fixnum, :default => 3600
 attribute :kafka_host, :kind_of => String
