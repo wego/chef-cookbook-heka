eeb344c425159a40f05cd93dea2b09685dfdc5ab
alex <uohzxela@gmail.com>
2015-05-20 11:54:02 +0800

    change to wego's repo for heka s3 output plugin

--- attributes/source.rb
+++ attributes/source.rb
@@ -8,7 +8,7 @@ default["heka"]["source"] = {
   "plugins" => [
     # Add items here like the following that's required for integration with kafka
     "add_external_plugin(git https://github.com/genx7up/heka-kafka acf3ac7a3d6d6dab313510f81828fca2f9375229)",
-    "add_external_plugin(git https://github.com/uohzxela/heka-s3 master)"
+    "add_external_plugin(git https://github.com/wego/heka-s3 master)"
   ],
   "git" => {
     "source" => "https://github.com/mozilla-services/heka.git",
@@ -22,6 +22,7 @@ default["heka"]["source"] = {
       ## Add libraries required for plugins such as the the sarama library
       ## for communication with kafka
       "github.com/Shopify/sarama",
+      ## for communication with AWS
       "github.com/mitchellh/goamz",
       "github.com/vaughan0/go-ini"
     ]
