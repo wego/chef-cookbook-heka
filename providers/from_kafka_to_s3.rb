035ba0cbfecd7ffeb03a8782dc4f5b9910121d21
alex <uohzxela@gmail.com>
2015-05-26 14:46:13 +0800

    add options for buffer configuration

--- providers/from_kafka_to_s3.rb
+++ providers/from_kafka_to_s3.rb
@@ -7,6 +7,8 @@ action :create do
   ticker_interval = new_resource.ticker_interval
   topic = new_resource.topic
   kafka_hosts = new_resource.kafka_hosts
+  buffer_path = new_resource.buffer_path
+  buffer_chunk_limit = new_resource.buffer_chunk_limit
 
   toml_file_path = "/etc/heka/from_kafka_#{topic}_to_s3.toml"
 
@@ -21,7 +23,9 @@ action :create do
       :region => region,
       :ticker_interval => ticker_interval,
       :topic => topic,
-      :kafka_hosts => kafka_hosts
+      :kafka_hosts => kafka_hosts,
+      :buffer_path => buffer_path,
+      :buffer_chunk_limit => buffer_chunk_limit
     )
   end
 end
--- resources/from_kafka_to_s3.rb
+++ resources/from_kafka_to_s3.rb
@@ -9,3 +9,5 @@ attribute :region, :kind_of => String
 attribute :topic, :kind_of => String, :name_attribute => true
 attribute :ticker_interval, :kind_of => Fixnum, :default => 3600
 attribute :kafka_hosts, :kind_of => Array
+attribute :buffer_chunk_limit, :kind_of => Fixnum, :default => 100000
+attribute :buffer_path, :kind_of => String
