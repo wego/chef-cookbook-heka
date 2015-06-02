8e3b3b4d4b3d0e07c67e09a2e8cf1fa61aff2f84
aranair <boa.homan@gmail.com>
2015-06-02 11:48:15 +0800

    Add bq plugin

--- attributes/source.rb
+++ attributes/source.rb
@@ -9,6 +9,7 @@ default["heka"]["source"] = {
     # Add items here like the following that's required for integration with kafka
     "add_external_plugin(git https://github.com/genx7up/heka-kafka acf3ac7a3d6d6dab313510f81828fca2f9375229)",
     "add_external_plugin(git https://github.com/uohzxela/heka-s3 master)"
+    "add_external_plugin(git https://github.com/aranair/heka-bigquery master)"
   ],
   "git" => {
     "source" => "https://github.com/mozilla-services/heka.git",
@@ -20,6 +21,11 @@ default["heka"]["source"] = {
     ],
     "packages" => [
       ## Add libraries required for plugins such as the the sarama library
+      ## for communication with bigquery
+      "golang.org/x/oauth2",
+      "golang.org/x/oauth2/google",
+      "golang.org/x/oauth2/jwt",
+      "google.golang.org/api/bigquery/v2",
       ## for communication with kafka
       "github.com/Shopify/sarama",
       ## for communication with AWS
