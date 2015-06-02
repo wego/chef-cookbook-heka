e8a4e91e76d7be7babef2dd28138177ea83c522e
aranair <boa.homan@gmail.com>
2015-06-02 11:23:00 +0800

    Heka bq

---
+++ providers/from_kafka_to_bq.rb
@@ -0,0 +1,31 @@
+action :create do
+  topic = new_resource.topic
+  kafka_hosts = new_resource.kafka_hosts
+
+  project_id = new_resource.project_id
+  dataset_id = new_resource.dataset_id
+  table_id = new_resource.table_id
+  project_id = new_resource.project_id
+  project_id = new_resource.project_id
+  
+  ticker_interval = new_resource.ticker_interval
+  buffer_path = new_resource.buffer_path
+  buffer_file = new_resource.buffer_file
+
+  toml_file_path = "/etc/heka/from_kafka_#{topic}_to_bq.toml"
+
+  template toml_file_path do
+    source "from_kafka_to_bq.toml.erb"
+    cookbook "heka"
+    variables(
+      :topic => topic,
+      :kafka_hosts => kafka_hosts,
+      :project_id => project_id,
+      :dataset_id => dataset_id,
+      :table_id => table_id,
+      :ticker_interval => ticker_interval,
+      :buffer_path => buffer_path,
+      :buffer_file => buffer_file
+    )
+  end
+end
---
+++ resources/from_kafka_to_bq.rb
@@ -0,0 +1,12 @@
+actions :create
+default_action :create
+
+attribute :topic, :kind_of => String, :name_attribute => true
+attribute :kafka_hosts, :kind_of => Array
+
+attribute :project_id, :kind_of => String
+attribute :dataset_id, :kind_of => String
+attribute :table_id, :kind_of => String
+attribute :ticker_interval, :kind_of => Fixnum, :default => 10
+attribute :buffer_path, :kind_of => String
+attribute :buffer_file, :kind_of => String
